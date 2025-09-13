785d5d876c0b8a078a468425773fd75d6fe0c1f7
SHUHEI Maejima <153809956+maeshu0619@users.noreply.github.com>
2025-09-13 15:47:15 +0900

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # ルーティング登録
+    db.init_app(app)
+
     from .routes import bp as main_bp
     app.register_blueprint(main_bp)
 
--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DB_DIR = os.environ.get("DB_DIR", os.path.join(BASE_DIR, "data"))  # ← data/ は作成される前提
 os.makedirs(DB_DIR, exist_ok=True)  # ← これが重要（起動時に確実に作る）
+
 class Config:
-    DB_PATH = os.path.join(DB_DIR, "dogfeed.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "change-me")
