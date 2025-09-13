dceee9e94b4cf160c412a311b3282268e62b46dd
SHUHEI Maejima <153809956+maeshu0619@users.noreply.github.com>
2025-09-13 15:58:19 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DB_DIR = os.environ.get("DB_DIR", os.path.join(BASE_DIR, "data"))  # ← data/ は作成される前提
-os.makedirs(DB_DIR, exist_ok=True)  # ← これが重要（起動時に確実に作る）
 
 class Config:
+    # Render の Environment に保存した DATABASE_URL を参照
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "change-me")
